2bc6d05ab646574fdc15e194a653dbad12b52b04
Hafiz123iftikhar <155850590+Hafiz123iftikhar@users.noreply.github.com>
2024-03-16 16:05:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let marks = prompt("Enter The Student's Marks Here: ");
+marks = Number(marks)
+
+
+if (marks >= 90) {
+
+            console.log("Grade: A");
+    } else if (marks >= 80) {
+            console.log("Grade: B");
+    } else if (marks >= 70) {
+            console.log("Grade: C");
+    } else if (marks >= 60) {
+            console.log("Grade: D");
+    } else {
+            console.log("Grade: F");
+            
+}
